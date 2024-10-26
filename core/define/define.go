8af803d1f911154df97ab7c83c1e8c8bd21c3a5c
iiitree <iiitree@users.noreply.github.com>
2024-10-26 15:38:48 +0800

    Initial commit

---
+++ core/define/define.go
@@ -0,0 +1,12 @@
+package define
+
+import "github.com/golang-jwt/jwt/v4"
+
+type UserClain struct {
+	Id       int
+	Identity string
+	Name     string
+	jwt.StandardClaims
+}
+
+var JwtKey = "tree-cloud-key"
---
+++ core/helper/helper.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"crypto/md5"
+	"fmt"
+	"tree-cloud/core/define"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func Md5(s string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(s)))
+}
+
+func GenerateToken(id int, identity string, name string) (string, error) {
+	uc := define.UserClain{
+		Id:       id,
+		Identity: identity,
+		Name:     name,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, uc)
+	tokenString, err := token.SignedString([]byte(define.JwtKey))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
